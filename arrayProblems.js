1055816025a73ccc9f63c2838832852cc93a1455
Lenovo <jannatjui414@gmail.com>
2025-09-01 21:43:26 +0600

    add new problem

--- arrayProblems.js
+++ arrayProblems.js
@@ -32,3 +32,18 @@ function createArray(n) {
 }
 
 console.log(createArray(5)); // [1, 2, 3, 4, 5]
+
+// Flatten Nested Arrays
+function flatten(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(flatten([1, [2, [3, 4]], 5])); // [1,2,3,4,5]
