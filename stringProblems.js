92425574bc24fa3fbb9b6f68321da4348d95af4d
Lenovo <jannatjui414@gmail.com>
2025-09-03 22:28:09 +0600

    add new string func

--- stringProblems.js
+++ stringProblems.js
@@ -73,3 +73,25 @@ function firstNonRepeatingChar(str) {
 console.log(firstNonRepeatingChar("programming")); // Output: "p"
 console.log(firstNonRepeatingChar("aabbcc")); // Output: null
 console.log(firstNonRepeatingChar("javascript")); // Output: "j"
+
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+// Example test
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
