da0cebe8eeee76530a4b1006320a80539152dbf7
Lenovo <jannatjui414@gmail.com>
2025-09-05 21:47:03 +0600

    add new array problem: find pair sum

--- arrayProblems.js
+++ arrayProblems.js
@@ -62,3 +62,22 @@ function findMissing(arr, n) {
 
 // Example (missing number is 4)
 console.log(findMissing([1, 2, 3, 5, 6], 6)); // Output: 4
+
+// Find all pairs in an array that add up to a given target number.
+function pairSum(arr, target) {
+  let pairs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        pairs.push([arr[i], arr[j]]);
+      }
+    }
+  }
+
+  return pairs;
+}
+
+// Example
+console.log(pairSum([1, 2, 3, 4, 5, 6], 7));
+// Output: [ [1,6], [2,5], [3,4] ]
