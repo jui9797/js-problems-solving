4e3086933533694565d4615ee13ca0dab99962d4
Lenovo <jannatjui414@gmail.com>
2025-09-30 20:14:11 +0600

    add two number related probles

--- numberProblems.js
+++ numberProblems.js
@@ -8,3 +8,28 @@ function checkNumber(num) {
 console.log(checkNumber(5)); // Positive
 console.log(checkNumber(-3)); // Negative
 console.log(checkNumber(0)); // Zero
+
+// Fibonacci Series (Recursion)
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(6)); // 8
+
+// Debounce Function (Interview Favorite)
+function debounce(fn, delay) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this, args), delay);
+  };
+}
+
+// Example
+const log = debounce(() => console.log("User stopped typing"), 1000);
+
+log();
+log();
+log();
+// Only runs once, 1 second after last call
