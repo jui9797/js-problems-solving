aac95b6f57671642bc8bed31fa8ed555167bec4d
Lenovo <jannatjui414@gmail.com>
2025-10-18 23:09:36 +0600

    add flatten func

--- stringProblems.js
+++ stringProblems.js
@@ -173,3 +173,14 @@ function isAnagram(a, b) {
   return true;
 }
 console.log(isAnagram("listen", "silent")); // true
+
+// Flatten Nested Array
+function flatten(arr) {
+  let result = [];
+  for (let item of arr) {
+    if (Array.isArray(item)) result = result.concat(flatten(item));
+    else result.push(item);
+  }
+  return result;
+}
+console.log(flatten([1, [2, [3, 4]], 5])); // [1,2,3,4,5]
