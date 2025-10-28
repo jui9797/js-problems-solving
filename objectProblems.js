67003682081fcb2f8a0f48acf05db81d26b0628e
Lenovo <jannatjui414@gmail.com>
2025-10-28 18:58:08 +0600

    add randomHexColor , deepClone func

--- objectProblems.js
+++ objectProblems.js
@@ -11,3 +11,35 @@ function objectIntersection(obj1, obj2) {
 }
 console.log(objectIntersection({ a: 1, b: 2, c: 3 }, { b: 2, c: 4 }));
 // {b:2}
+
+// Generate Random Hex Color
+
+function randomHexColor() {
+  let hex = "#";
+  let chars = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++) {
+    hex += chars[Math.floor(Math.random() * 16)];
+  }
+  return hex;
+}
+console.log(randomHexColor()); // Example: #A1B2C3
+
+// Deep Clone an Object
+function deepClone(obj) {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    return obj.map(deepClone);
+  }
+  const clonedObj = {};
+  for (let key in obj) {
+    clonedObj[key] = deepClone(obj[key]);
+  }
+  return clonedObj;
+}
+const original = { a: 1, b: { c: 2 } };
+const cloned = deepClone(original);
+cloned.b.c = 3;
+// console.log(original.b.c); // 2
+// console.log(cloned.b.c);   // 3
