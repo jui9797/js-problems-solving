61b78f7f063aa75316aefa04d14a783e11d24304
Lenovo <jannatjui414@gmail.com>
2025-11-01 23:41:57 +0600

    add new func

--- stringProblems.js
+++ stringProblems.js
@@ -304,3 +304,9 @@ console.log(isPalindrome("racecar"));
 // true
 console.log(isPalindrome("hello"));
 // false
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log(reverseString("hello")); // "olleh"
+console.log(reverseString("JavaScript")); // "tpircSavaJ"
