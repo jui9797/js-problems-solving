2a36e1729154557b08b4752e4aa05d48112179c6
Lenovo <jannatjui414@gmail.com>
2025-11-07 10:56:05 +0600

    add arrayDifference func

--- arrayProblems.js
+++ arrayProblems.js
@@ -148,3 +148,18 @@ function sumEven(arr) {
   return sum;
 }
 console.log(sumEven([1, 2, 3, 4, 5, 6])); // 12
+
+//Find the Difference Between Two Arrays
+
+function arrayDifference(arr1, arr2) {
+  let diff = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) diff.push(arr1[i]);
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    if (!arr1.includes(arr2[i])) diff.push(arr2[i]);
+  }
+  return diff;
+}
+console.log(arrayDifference([1, 2, 3], [2, 3, 4]));
+// Output: [1, 4]
