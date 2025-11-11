e6c5ab3f0b67424f8fad5341f92b4e9d15e35e24
Lenovo <jannatjui414@gmail.com>
2025-11-11 11:09:23 +0600

    add removeFalsy func

--- arrayProblems.js
+++ arrayProblems.js
@@ -192,3 +192,15 @@ function mergeSortedArrays(a, b) {
 }
 console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6]));
 // Output: [1, 2, 3, 4, 5, 6]
+
+//Remove Falsy Values from an Array
+
+function removeFalsy(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) result.push(arr[i]);
+  }
+  return result;
+}
+console.log(removeFalsy([0, 1, false, 2, "", 3, null]));
+// Output: [1, 2, 3]
